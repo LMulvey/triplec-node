2ee03aa3853fd9ce51419a2d598e9e4db085a388
Lee Mulvey <lmulvey@me.com>
2017-11-05 21:07:47 +0000

    Now serving: static version of the website ADDED: 'public' folder containing static assets for website REMOVED: all traces of the PHP build version

---
+++ server/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Basic express setup:
+const PORT          = process.env.PORT || 8080;
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const app           = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Routes
+const adminRoutes = require('./routes/admin.js');
+const galleryRoutes = require('./routes/gallery.js');
+
+// Route those Routes
+//app.use("/admin", adminRoutes);
+//app.use("/gallery-temp", galleryRoutes);
+
+// TO-DO: change route to /gallery once gallery feature is completed.
+
+app.listen(PORT, () => {
+  console.log("TripleC website now live on port " + PORT);
+});
