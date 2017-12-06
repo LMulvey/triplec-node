11f00f179e43989756979e03ff0f3251b9b56ef4
Lee Mulvey <lmulvey@me.com>
2017-12-06 03:47:33 +0000

    Updated production Mongo URI

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // Mongoose configuration
 module.exports = {
     url: 'mongodb://localhost/triplec',
-    url_production: `mongodb://${process.env.MONGO_URI}/${process.env.DB_NAME}`
-};
+    url_production: `mongodb://${process.env.DB_USER}:mongodb://${process.env.DB_PASS}@${process.env.MONGO_URI}/${process.env.DB_NAME}`
+}; 
