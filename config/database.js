9a1ff66115df3c96b7c5dc924b1290da28747041
Lee Mulvey <lmulvey@me.com>
2017-12-06 04:11:16 +0000

    Fixing.

--- config/database.js
+++ config/database.js
@@ -1,7 +1,10 @@
 // Mongoose configuration
 const dotenv        = require('dotenv').config();
+let user = encodeURIComponent(process.env.DB_USER);
+let pass = encodeURIComponent(process.env.DB_PASS);
+let db = encodeURIComponent(process.env.DB_NAME);
 
 module.exports = {
     url: 'mongodb://localhost/triplec',
-    url_production: process.env.PRODUCTION_URI
+    url_production: `mongodb://${user}:${pass}@127.0.0.1/${db}?authSource=admin`
 }; 
