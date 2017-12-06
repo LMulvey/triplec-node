63e71828a3ee6d876bfdfd75c4be6143bfbdfbc4
Lee Mulvey <lmulvey@me.com>
2017-12-06 03:49:04 +0000

    Goofed up dotenv.

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 // Mongoose configuration
+const dotenv        = require('dotenv').config();
+
 module.exports = {
     url: 'mongodb://localhost/triplec',
     url_production: `mongodb://${process.env.DB_USER}:mongodb://${process.env.DB_PASS}@${process.env.MONGO_URI}/${process.env.DB_NAME}`
