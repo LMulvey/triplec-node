2d0d32713d908bdc203116e54521a60b4137f3e9
Lee Mulvey <lmulvey@me.com>
2017-12-06 03:58:46 +0000

    Issues with connectivity on DO

--- config/database.js
+++ config/database.js
@@ -3,5 +3,5 @@ const dotenv        = require('dotenv').config();
 
 module.exports = {
     url: 'mongodb://localhost/triplec',
-    url_production: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.MONGO_URI}/${process.env.DB_NAME}`
+    url_production: process.env.PRODUCTION_URI
 }; 
