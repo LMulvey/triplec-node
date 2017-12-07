9ce40d9a577c2eff5974ff59d1e8ef482cd6d054
Lee Mulvey <lmulvey@me.com>
2017-12-07 02:05:38 +0000

    Setup a better development environment so I would stop having to change Mongo URIs. Adjusted choclat.css to look nicer with the layout.

--- config/database.js
+++ config/database.js
@@ -3,8 +3,12 @@ const dotenv        = require('dotenv').config();
 let user = encodeURIComponent(process.env.DB_USER);
 let pass = encodeURIComponent(process.env.DB_PASS);
 let db = encodeURIComponent(process.env.DB_NAME);
+let url;
 
-module.exports = {
-    url: 'mongodb://localhost/triplec',
-    url_production: `mongodb://${user}:${pass}@127.0.0.1/${db}?authSource=admin`
-}; 
+if(process.env.NODE_ENV == 'development') {
+    url = 'mongodb://localhost/triplec';
+} else {
+    url = `mongodb://${user}:${pass}@127.0.0.1/${db}?authSource=admin`;
+}
+
+module.exports = {url}; 
--- routes/gallery.js
+++ routes/gallery.js
@@ -6,6 +6,7 @@ const Gallery = require('../models/galleries');
 const Photo = require('../models/photos');
 
 galleryRoutes.get('/', (req, res) => {
+    config.defaultTemplateVars.isHome = false;
     res.render('gallery_main', {config: config.defaultTemplateVars});
 });
 
@@ -15,9 +16,10 @@ galleryRoutes.get('/:url', (req, res) => {
             if(results) {
                 Photo.find({ 'relation.gallery_id' : results._id }, (err, photos) => {
                     if(err) throw err;
+                    config.defaultTemplateVars.isHome = false;
                     config.defaultTemplateVars.pageTitle = 
                     `${results.info.name} - ${results.info.description} | Triple C Woodworx`;
-                    
+
                     res.render('gallery_view', {
                         config: config.defaultTemplateVars,
                         gallery: results,
